0d9a5740687d9c688fadda6cccb5a5328a46b7e9
gorm121 <159934746+gorm121@users.noreply.github.com>
2025-11-17 16:23:55 +0300

    Delete src/main/java/FileRecord.java

--- src/main/java/FileRecord.java
+++
@@ -1,9 +0,0 @@
-public class FileRecord {
-    String owner;
-    String filePath;
-
-    public FileRecord(String owner, String filePath) {
-        this.owner = owner;
-        this.filePath = filePath;
-    }
-}
